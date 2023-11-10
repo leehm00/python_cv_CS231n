511bbdef2a5642281443a924cd528e01a193bdfe
leehm00 <leehm00@qq.com>
2023-11-10 17:38:28 +0800

    initail commit

---
+++ 1_python/1_basic_data_types/strings.py
@@ -0,0 +1,19 @@
+hello = 'hello'    # String literals can use single quotes
+world = "world"    # or double quotes; it does not matter.
+print(hello)       # Prints "hello"
+print(len(hello))  # String length; prints "5"
+hw = hello + ' ' + world  # String concatenation
+print(hw)  # prints "hello world"
+hw12 = '%s %s %d' % (hello, world, 12)  # sprintf style string formatting
+print(hw12)  # prints "hello world 12"
+
+# functions
+# find more in https://docs.python.org/3.5/library/stdtypes.html#string-methods
+s = "hello"
+print(s.capitalize())  # Capitalize a string; prints "Hello"
+print(s.upper())       # Convert a string to uppercase; prints "HELLO"
+print(s.rjust(7))      # Right-justify a string, padding with spaces; prints "  hello"
+print(s.center(7))     # Center a string, padding with spaces; prints " hello "
+print(s.replace('l', '(ell)'))  # Replace all instances of one substring with another;
+                                # prints "he(ell)(ell)o"
+print('  world '.strip())  # Strip leading and trailing whitespace; prints "world"
