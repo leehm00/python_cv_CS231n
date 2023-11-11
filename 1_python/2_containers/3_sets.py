350366b0de4e37de2836a562ab79240381be94bb
leehm00 <leehm00@qq.com>
2023-11-11 22:53:17 +0800

    add sets

--- 1_python/2_containers/3_sets.py
+++ 1_python/2_containers/3_sets.py
@@ -8,3 +8,14 @@ animals.add('cat')        # Adding an element that is already in the set does no
 print(len(animals))       # Prints "3"
 animals.remove('cat')     # Remove an element from a set
 print(len(animals))       # Prints "2"
+
+# loops
+animals = {'cat', 'dog', 'fish'}
+for idx, animal in enumerate(animals):
+    print('#%d: %s' % (idx + 1, animal))
+# Prints "#1: fish", "#2: dog", "#3: cat"
+
+# set comprehension
+from math import sqrt
+nums = {int(sqrt(x)) for x in range(30)}
+print(nums)  # Prints "{0, 1, 2, 3, 4, 5}"
