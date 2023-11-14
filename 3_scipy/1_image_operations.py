ef28fb4db7bca5b392a5b2e5945d2f8b477ef80d
leehm00 <leehm00@qq.com>
2023-11-14 14:59:21 +0800

    add 1 tint

--- 3_scipy/1_image_operations.py
+++ 3_scipy/1_image_operations.py
@@ -1,6 +1,10 @@
-from scipy.misc import  imresize
-from imageio import imread, imsave
-import cv2
+# from scipy.misc import imresize
+# import imageio.v2 as imageio
+from skimage.transform import resize
+from imageio.v2 import imread
+# from keras.preprocessing.image import save_img
+from keras.utils import image_utils
+# import cv2
 # Read an JPEG image into a numpy array
 img = imread('./assets/cat.jpg')
 print(img.dtype, img.shape)  # Prints "uint8 (400, 248, 3)"
@@ -14,7 +18,8 @@ print(img.dtype, img.shape)  # Prints "uint8 (400, 248, 3)"
 img_tinted = img * [1, 0.95, 0.9]
 
 # Resize the tinted image to be 300 by 300 pixels.
-img_tinted = imresize(img_tinted, (300, 300))
+img_tinted = resize(img_tinted, (300, 300))
+print(img_tinted.dtype, img_tinted.shape)
 
-# Write the tinted image back to disk
-imsave('assets/cat_tinted.jpg', img_tinted)
+# # Write the tinted image back to disk
+image_utils.save_img('assets/cat_tinted.jpg', img_tinted)
